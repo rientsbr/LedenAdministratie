6b37450d8d2e0a569f065d30ddc497aedc46c9fd
Rients Brandsma <37273415+rientsbr@users.noreply.github.com>
2020-10-09 21:22:17 +0200

    Update utils.py

--- LedenAdministratie/LedenAdministratie/utils.py
+++ LedenAdministratie/LedenAdministratie/utils.py
@@ -1,55 +1,35 @@
-"""LedenAdministratie URL Configuration
+from django.core.mail import EmailMessage
+from django.utils import timezone
+from .models import Email, Setting
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.8/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Add an import:  from blog import urls as blog_urls
-    2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
-"""
-from django.urls import path, re_path, include
-from django.contrib import admin
-from . import views, api
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    re_path(r'^tinymce/', include('tinymce.urls')),
-    path('members/<filter_slug>/', views.MemberListView.as_view(), name='members'),
-    path('members/', views.MemberListView.as_view(), name='members'),
-    path('logoff/', views.LogoffView.as_view(), name='logoff'),
-    path('export/', views.ExportView.as_view(), name='export'),
-    path('member/edit/<int:pk>/', views.MemberUpdateView.as_view(), name='lid_edit'),
-    path('member/delete/<int:pk>/', views.MemberDeleteView.as_view(), name='lid_delete'),
-    path('member/create/', views.MemberCreateView.as_view(), name='lid_create'),
-    path('member/addnote/<int:member_id>/', views.MemberAddNoteView.as_view(), name='lid_addnote'),
-    path('member/delnote/<int:pk>', views.MemberDeleteNoteView.as_view(), name='lid_delnote'),
-    path('member/editnote/<int:pk>', views.MemberEditNoteView.as_view(), name='lid_editnote'),
-    path('todolist/', views.TodoListView.as_view(), name='todolist'),
-    path('invoice/create/', views.InvoiceCreateView.as_view(), name='invoice_create'),
-    path('invoice/create/<int:member_id>/', views.InvoiceCreateView.as_view(), name='invoice_create_for'),
-    path('invoice/display/<int:pk>', views.InvoiceDisplayView.as_view(), name='invoice_display'),
-    path('invoice/delete/<int:pk>', views.InvoiceDeleteView.as_view(), name='invoice_delete'),
-    path('invoice/payment/', views.InvoicePaymentView.as_view(), name='invoice_payment'),
-    path('invoice/pay_full/<int:pk>', views.InvoicePayFullView.as_view(), name='invoice_pay_full'),
-    path('invoice/pay_part/<int:pk>', views.InvoicePayPartView.as_view(), name='invoice_pay_part'),
-    path('invoice/pay_part/<int:pk>/<int:member_id>', views.InvoicePayPartView.as_view(), name='invoice_pay_part'),
-    path('invoice/send/', views.InvoiceSendView.as_view(), name='invoice_send'),
-    path('email/send/', views.EmailSendView.as_view(), name='email_send'),
-    path('email/log/', views.EmailLogView.as_view(), name='email_log'),
-    path('settings/', views.SettingsView.as_view(), name='settings'),
-    path('api/v1/smoelenboek/', api.ApiV1Smoelenboek.as_view()),
-    path('api/v1/smoelenboek/<int:pk>/', api.ApiV1SmoelenboekUser.as_view()),
-    path('api/v1/smoelenboek/<str:day>/', api.ApiV1Smoelenboek.as_view()),
-    path('api/v1/idp/details/<str:fields>', api.ApiV1IDPGetDetails.as_view()),
-    path('api/v1/idp/verify/<str:fields>', api.ApiV1IDPVerify.as_view()),
-    path('api/v1/idp/avatar', api.ApiV1IDPAvatar.as_view()),
-    path('aanmelden/', views.LidAanmeldView.as_view(), name='lid_aanmelden'),
-#    re_path(r'^aanmelden_ok/$', views.aanmelden_ok, name='aanmelden_ok'),
-    path('', views.LoginView.as_view(), name='login'),
-]
+class Utils:
+    # Send and e-mail, and log it in the email history table
+    @staticmethod
+    def send_email(message, username, member):
+        log = Email()
+        log.member = member
+        log.subject = message.subject
+        log.recipients = ",".join(message.recipients())
+        log.sent_by = username
+        log.sent = timezone.now()
+        count = 0
+        try:
+            count = message.send(fail_silently=False)
+            if count == 1:
+                log.status = 'OK'
+            else:
+                log.status = 'Fout bij versturen!'
+        except Exception as e:
+            log.status = "Fout bij versturen: {0}".format(str(e))
+
+        log.save_base()
+        return count == 1
+
+    @staticmethod
+    def get_setting(name):
+        try:
+            setting = Setting.objects.get(name=name)
+        except Setting.DoesNotExist:
+            return ""
+        return setting.value
