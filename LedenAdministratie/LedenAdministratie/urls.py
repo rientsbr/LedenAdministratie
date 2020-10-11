7768142002407ea07887b70f60866b6964f388cd
Rients Brandsma <37273415+rientsbr@users.noreply.github.com>
2020-10-11 14:02:20 +0200

    aaa

--- LedenAdministratie/LedenAdministratie/urls.py
+++ LedenAdministratie/LedenAdministratie/urls.py
@@ -52,6 +52,6 @@ urlpatterns = [
     path('api/v1/idp/avatar', api.ApiV1IDPAvatar.as_view()),
     path('aanmelden/', views.LidAanmeldView.as_view(), name='lid_aanmelden'),
 #    re_path(r'^aanmelden_ok/$', views.aanmelden_ok, name='aanmelden_ok'),
-#    path('', views.LoginView.as_view(), name='login'),
-    path('', views.login, name='login'),
+    path('', views.LoginView.as_view(), name='login'),
+#    path('', views.login, name='login'),
 ]
